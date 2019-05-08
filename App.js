8b7e8fe0a5a37360061bca2d16fbc986db6e1cf1
NCJo <cnimkulr@gmail.com>
2019-05-08 02:05:35 -0600

    Added: Azure chat bot functionalities

--- App.js
+++ App.js
@@ -1,49 +1,73 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { GiftedChat } from "react-native-gifted-chat";
+import { DirectLine } from "botframework-directlinejs";
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+const directLine = new DirectLine ({
+  secret: "0sTl4Rzyh4M.IMyBcEs3d-uFr4po02elq-rWRRMmArTELDZCK1kVUgQ"
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+const botMessageToGiftedMessage = botMessage => ({
+  ...botMessage,
+  _id: botMessage.id,
+  createdAt: botMessage.timestamp,
+  user: {
+    _id: 2,
+    name: "React Native",
+    // avatar
+  }
+});
+
+function giftedMessageToBotMessage(message) {
+  return {
+    from: { id: 1, name: "John Doe" },
+    type: "message",
+    text: message.text
+  };
+}
+
+export default class App extends Component {
+  state = {
+    messages: []
+  };
+
+  constructor(props) {
+    super(props);
+    directLine.activity$.subscribe(botMessage => {
+      const newMessage = botMessageToGiftedMessage(botMessage);
+      this.setState({ messages: [newMessage, ...this.state.messages] });
+    });
+  }
+
+  onSend = messages => {
+    this.setState({ messages: [...messages, ...this.state.messages] });
+
+    messages.forEach(message => {
+      directLine
+        .postActivity(giftedMessageToBotMessage(message))
+        .subscribe(() => console.log("success"), () => console.log("failed"));
+    });
+  };
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <GiftedChat 
+        user={{
+          _id:1
+        }}
+        messages={this.state.messages}
+        onSend={this.onSend}
+      />
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    flex: 1
+  }
 });
+
+
+// 0sTl4Rzyh4M.IMyBcEs3d-uFr4po02elq-rWRRMmArTELDZCK1kVUgQ
+// 0sTl4Rzyh4M.FWH5X7qu3o2QwW5oVn170OAd2Oqmgy88RjlreWrtJjw
